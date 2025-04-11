1cee9544d4b328cc312f67d4f57aa5827c85f3f3
sparkle-mind <samirpatel7095@gmail.com>
2025-04-11 21:46:20 +0530

    working

--- src/components/banner/Banner.jsx
+++ src/components/banner/Banner.jsx
@@ -9,15 +9,7 @@ const Banner = () => {
         <div className="container">
           <div className="banner-inner">
             <div className="row flex align-center justify-between">
-              <h1>h1</h1>
-              <h2>h2</h2>
-              <h3>h3</h3>
-              <h4>h4</h4>
-              <h5>h5</h5>
-              <h6>h6</h6>
-              <p>p</p>
-              <DownloadBtn />
-              <GradientBtn />
+            
             </div>
           </div>
         </div>
